874404ff95a184c93e10657b84f93b1e94556256
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2023-01-14 11:06:08 -0500

    setting up files

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+        origin: 'http://localhost:3000'
+}));
+
+//requiring mongoose needs to be before the routes for database to connect
+require('./config/mongoose.config'); 
+// require('./routes/product.routes')(app); 
+
+app.listen(8000, () => {
+    console.log(`Listening on port: 8000`); 
+})
